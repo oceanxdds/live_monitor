dedc01b9d0349e307944183dc42a3b1a81dd7e64
oceanxdds <oceanxdds@gmail.com>
2019-06-22 13:21:20 +0800

    update

--- src/js/app.vue
+++ src/js/app.vue
@@ -45,7 +45,7 @@
                         @dragleave="dragleave_handler(v)"
                         @drop="drop_handler(v)"
                         
-                        @click="dragclick_handler(v)">
+                        @mouseleave="dragend_handler(v)">
                         <b-dropdown class="mx-1" size="sm" split :text="'CH'+ (v.order+1) " variant="success" @click="focusVideo(v)">
                             <b-dropdown-text style="width:275px">
                                 <b-form-input size="sm" :value="v.code" disabled></b-form-input>
@@ -253,9 +253,6 @@ export default {
             video.dragover = true;
         },
         dragleave_handler:function(video){
-            video.dragover = false;
-        },
-        dragclick_handler:function(video){
             video.dragover = false;
         },
         drop_handler:function(video){
